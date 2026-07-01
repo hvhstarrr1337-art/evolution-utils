d34c8390e12b7c396976dc581971425d75645eeb
hvhstarrr1337-art <hvhstarrr1337@gmail.com>
2026-07-01 06:24:46 +0300

    Update library.lua

--- library.lua
+++ library.lua
@@ -1954,7 +1954,7 @@ function pages:section(props)
 	)
 	--
 	layout:GetPropertyChangedSignal("AbsoluteContentSize"):Connect(function()
-		sectionholder.Size = UDim2.new(1,0,0,layout.AbsoluteContentSize.Y + 35)
+		sectionholder.Size = UDim2.new(1,0,0,layout.AbsoluteContentSize.Y + 55)
 	end)
 	-- // section tbl
 	section = {
